ed48628ff5c4cdf8fb995d8496733c5460c995c2
Mahesh <maheshsable33@gmail.com>
2024-08-25 23:44:18 +0530

    exception handling and user authorization logic commited

--- proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
@@ -1,6 +1,5 @@
 package com.upgrad.proman.service.business;
 
-import com.upgrad.proman.service.dao.UserDao;
 import com.upgrad.proman.service.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SignupBusinessService {
-    @Autowired
-    private UserDao userDao;
 
+    @Autowired
+    private UserAdminBusinessService userAdminBusinessService;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity signup(UserEntity userEntity) {
-        return userDao.createUser(userEntity);
+        return userAdminBusinessService.createUser(userEntity);
     }
+
 }
+
+
